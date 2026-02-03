0f383448f32286a9a5dddd28869481155dddce25
Alexcb7 <alex.cortell.ifp@gmail.com>
2026-02-03 10:36:55 +0100

    dsadasdas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithub = process.env.DEPLOY_TARGET === "github";
+const repo = "mi-app-next";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  ...(isGithub
+    ? { basePath: `/${repo}`, assetPrefix: `/${repo}/` }
+    : {}),
 };
 
 export default nextConfig;
